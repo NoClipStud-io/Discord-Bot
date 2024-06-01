794612d6352c7d972859753c9fa92f15e516c50e
No Clip Studio <136483947+NoClipStud-io@users.noreply.github.com>
2024-06-01 18:05:10 -0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,3 @@
-import requests
-import os
-import time
 from flask import Flask
 from threading import Thread
 
@@ -11,25 +8,8 @@ def home():
     return "I'm alive"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)#8080
+  app.run(host='0.0.0.0',port=8080)
 
-def check_availability():
-    while True:
-        try:
-            r = requests.get('http://localhost:8080')#8080
-            if r.status_code == 200:
-                print('Bot is online.')
-            else:
-                print('Bot is offline. Restarting...')
-                os.system('sudo ifconfig eth0 down && sudo ifconfig eth0 up')
-                Thread(target=run).start()
-        except:
-            print('Bot is offline. Restarting...')
-            os.system('sudo ifconfig eth0 down && sudo ifconfig eth0 up')
-            Thread(target=run).start()
-        time.sleep(10)
-
-def keep_alive():
+def keep_alive():  
     t = Thread(target=run)
     t.start()
-    Thread(target=check_availability).start()
